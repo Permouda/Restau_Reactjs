260907d31c52b398a3e5f86fd60cc672d254a4bc
Permouda <92ilya@gmail.com>
2024-07-25 17:43:32 +0100

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import HeroSection from './components/HeroSection'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline bg-white">
-      Hello world!
-    </h1>
+    <div>
+      <HeroSection />
+    </div>
   )
 }
 
